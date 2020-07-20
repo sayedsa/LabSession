c5de67feadd221dd2e303f530ebf9a8ccf7588c1
Snoopy <sadatsayedibrahim@gmail.com>
2020-07-20 16:23:17 -0700

    First Commit

---
+++ MavenDemo2/src/main/java/Lab/LabSession.java
@@ -0,0 +1,10 @@
+package Lab;
+
+public class LabSession {
+
+	public static void main(String[] args) {
+		System.out.println(" Today is lab day");
+
+	}
+
+}
---
+++ MavenDemo2/src/test/java/MavenDemo2/MavenDemo2/AppTest.java
@@ -0,0 +1,20 @@
+package MavenDemo2.MavenDemo2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
